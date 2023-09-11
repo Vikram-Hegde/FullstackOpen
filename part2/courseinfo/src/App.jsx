5a433f7db53875e2f70f0bdcfb82f72915cc6aaf
Vikram-Hegde <vikramhegde1440@gmail.com>
2023-09-11 09:31:03 +0530

    2.5 :check

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,44 +1,4 @@
-
-const Header = ({ course }) => {
-	return <h1>{course}</h1>
-}
-
-const Part = ({ part, exercise }) => {
-	return (
-		<p>
-			{part} {exercise}
-		</p>
-	)
-}
-
-const Content = ({ parts }) => {
-	return (
-		<>
-			{parts.map((item) => (
-				<Part key={item.name} part={item.name} exercise={item.exercises} />
-			))}
-		</>
-	)
-}
-
-const Total = ({ parts }) => {
-	return (
-		<b>
-			Total of {parts.reduce((acc, exercise) => acc + exercise.exercises, 0)}{' '}
-			exercises
-		</b>
-	)
-}
-
-const Course = ({ courses }) => {
-	return courses.map((course) => (
-		<section key={course.id}>
-			<Header course={course.name} />
-			<Content parts={course.parts} />
-			<Total parts={course.parts} />
-		</section>
-	))
-}
+import Course from './components/Course'
 
 const App = () => {
 	const courses = [
