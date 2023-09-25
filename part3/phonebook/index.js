06346502cbfc110c972b5c5a2ca2eca220133d79
Vikram-Hegde <vikramhegde1440@gmail.com>
2023-09-25 10:00:40 +0530

    init and 3.1 assignment ✅

---
+++ part3/phonebook/index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+
+const phoneBook = [
+	{
+		id: 1,
+		name: 'Arto Hellas',
+		number: '040-123456',
+	},
+	{
+		id: 2,
+		name: 'Ada Lovelace',
+		number: '39-44-5323523',
+	},
+	{
+		id: 3,
+		name: 'Dan Abramov',
+		number: '12-43-234345',
+	},
+	{
+		id: 4,
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122',
+	},
+]
+
+const app = express()
+
+app.get('/api/persons', (request, response) => {
+	response.json(phoneBook)
+})
+
+app.listen(3000, () => {
+	console.log('Server is running on port 3000')
+})
