0082836abbd6f43ab1234afcf4695cb95181e21e
Vikram-Hegde <vikramhegde1440@gmail.com>
2023-09-25 11:23:38 +0530

    3.3 - 3.6 assignment ✅

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import crypto from 'crypto'
+import { notStrictEqual } from 'assert'
+import { log } from 'console'
 
-const phoneBook = [
+let phoneBook = [
 	{
 		id: 1,
 		name: 'Arto Hellas',
@@ -23,7 +26,14 @@ const phoneBook = [
 	},
 ]
 
+const generateID = () => {
+	const maxID = Math.max(...phoneBook.map((person) => person.id))
+	return maxID + 1
+}
+
 const app = express()
+
+app.use(express.json())
 
 app.get('/api/persons', (request, response) => {
 	response.json(phoneBook)
@@ -34,6 +44,50 @@ app.get('/info', (request, response) => {
 	<p>Phonebook has info for ${phoneBook.length} people</p>	
 	<p>${new Date()}</p>
 	`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	const note = phoneBook.find((person) => person.id === id)
+
+	if (!note) {
+		response.sendStatus(404)
+	}
+
+	response.json(note)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	const note = phoneBook.find((person) => person.id === id)
+	if (!note) return response.sendStatus(404)
+	phoneBook = phoneBook.filter((person) => person.id !== id)
+	response.sendStatus(204)
+})
+
+app.post('/api/persons', (request, response) => {
+	const { body } = request
+
+	if (!body || !body.name || !body.number) {
+		return response.status(400).json({
+			error: 'content missing',
+		})
+	}
+
+	const alreadyExists = phoneBook.find((person) => person.name === body.name)
+	if (alreadyExists) {
+		return response.status(409).json({
+			error: 'name must be unique',
+		})
+	}
+
+	const newEntry = {
+		...body,
+		id: generateID(),
+	}
+	phoneBook.push(newEntry)
+
+	response.json(newEntry)
 })
 
 app.listen(3000, () => {
