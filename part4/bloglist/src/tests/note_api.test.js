b301c00d9c89825afe37291e9d755604c0debd38
Vikram-Hegde <vikramhegde1440@gmail.com>
2023-10-02 10:06:26 +0530

    4.10 assignment ✅

--- part4/bloglist/src/tests/note_api.test.js
+++ part4/bloglist/src/tests/note_api.test.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 import supertest from 'supertest'
 import { app } from '../index.js'
+import { Blog } from '../models/Blog.js'
 
 const api = supertest(app)
 
@@ -16,6 +17,22 @@ test('blogs have id defined', async () => {
 	expect(response.status).toBe(200)
 	response.body.forEach((res) => expect(res.id).toBeDefined())
 }, 8000)
+
+test('post request adds to db', async () => {
+	const beforePost = await api.get('/api/blogs')
+	expect(beforePost.status).toBe(200)
+	const newBlog = {
+		title: 'React patterns',
+		author: 'Michael Chan',
+		url: 'https://reactpatterns.com/',
+		likes: 7,
+	}
+
+	await Blog.create(newBlog)
+
+	const afterPost = await api.get('/api/blogs')
+	expect(afterPost.body.length).toBe(beforePost.body.length + 1)
+}, 15000)
 
 afterAll(async () => {
 	await mongoose.connection.close()
